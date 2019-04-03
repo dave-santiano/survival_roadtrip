cf55429995b9d74078114a6d599c8bb0f85c660d
armytag <armitageth@gmail.com>
2019-04-03 16:41:02 +0800

    Changed Car to KinematicBody2D

--- Car.gd
+++ Car.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
-export (float) var speed
+export (float) var ACCELERATION
+export (float) var TURNING
+
+var speed = 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,16 +16,19 @@ func _process(delta):
 	var angle = 0.0
 	
 	if Input.is_action_pressed("ui_up"):
-		speed += 0.1
+		speed += ACCELERATION
 	if Input.is_action_pressed("ui_down"):
-		speed -= 0.1
+		speed -= ACCELERATION
 		
 	if Input.is_action_pressed("ui_left"):
-		angle -= 0.01
+		angle -= TURNING
 	if Input.is_action_pressed("ui_right"):
-		angle += 0.01
+		angle += TURNING
 
 	rotation += angle;
 	var velocity = Vector2(cos(-rotation + PI/2) * speed, -sin(-rotation + PI/2) * speed)
-	position += velocity;
-	
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+        var collision_point = collision_info.position
+
+    
